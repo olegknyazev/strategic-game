f0efa476278afabcb2c9f056011a30f2383e295e
Oleg Knyazev <knyazev751@gmail.com>
2018-05-14 21:21:59 +0300

    Implement rendering of static world state

--- Client/Assets/Scripts/Client.cs
+++ Client/Assets/Scripts/Client.cs
@@ -3,16 +3,33 @@ using System.Net;
 using System.Threading;
 using UnityEngine;
 using StrategicGame.Common;
+using System.Collections.Generic;
 
 namespace StrategicGame.Client {
     public class Client : MonoBehaviour {
+        public World WorldPrefab;
+        public Camera Camera;
+
         Thread _thread;
         bool _running;
+        List<Message> _incomingMessages = new List<Message>();
+        World _world;
 
         void Start() {
             _running = true;
             _thread = new Thread(RemoteThread);
             _thread.Start();
+        }
+
+        void Update() {
+            List<Message> messagesToProcess = new List<Message>();
+            lock (_incomingMessages) {
+                messagesToProcess.AddRange(_incomingMessages);
+                _incomingMessages.Clear();
+            }
+            foreach (var msg in messagesToProcess)
+                if (msg is WorldParameters)
+                    RecreateWorld((WorldParameters)msg);
         }
 
         void OnDisable() {
@@ -23,13 +40,25 @@ namespace StrategicGame.Client {
             }
         }
 
-        private void RemoteThread() {
+        void RecreateWorld(WorldParameters worldParams) {
+            if (_world) {
+                DestroyImmediate(_world.gameObject);
+                _world = null;
+            }
+            _world = GameObject.Instantiate(WorldPrefab, transform);
+            _world.Initialize(worldParams);
+            Camera.transform.position = _world.Center + Vector3.up * 10;
+            Camera.transform.LookAt(_world.Center);
+        }
+
+        void RemoteThread() {
             var endPoint = new IPEndPoint(IPAddress.Loopback, 4040);
             var remoteServer = RemoteServer.TryConnect(endPoint, UnityConsoleLogger.Instance);
             while (_running) {
                 Message msg;
                 while ((msg = remoteServer.ReadMessage()) != null)
-                    Debug.LogFormat("Message received: {0}", msg);
+                    lock (_incomingMessages)
+                        _incomingMessages.Add(msg);
                 Thread.Sleep(10);
             }
         }
---
+++ Client/Assets/Scripts/World.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using StrategicGame.Common;
+
+namespace StrategicGame.Client {
+    public class World : MonoBehaviour {
+        public GameObject CellPrefab;
+
+        WorldParameters _params;
+
+        public Vector3 Center {
+            get { return new Vector3(_params.Width / 2f, 0, _params.Height / 2f); }
+        }
+
+        public void Initialize(WorldParameters worldParams) {
+            Debug.Assert(_params == null);
+            _params = worldParams;
+            for (int x = 0; x < worldParams.Width; ++x)
+                for (int z = 0; z < worldParams.Height; ++z)
+                    GameObject.Instantiate(CellPrefab, new Vector3(x, 0, z), Quaternion.identity, transform);
+        }
+    }
+}
