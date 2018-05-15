b435277610ee7e73455431ee95628aff6beb569e
Oleg Knyazev <knyazev751@gmail.com>
2018-05-15 14:50:21 +0300

    Add highlighting of selected units

--- Client/Assets/Scripts/Selectable.cs
+++ Client/Assets/Scripts/Selectable.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using UnityEngine;
-using UnityEngine.Assertions;
+﻿using UnityEngine;
 
 namespace StrategicGame.Client {
     public class Selectable : MonoBehaviour {
+        public Material NormalMaterial;
+        public Material SelectedMaterial;
+
         bool _selected;
+        MeshRenderer _materialTarget;
+
+        void Awake() {
+            _materialTarget = GetComponentInChildren<MeshRenderer>();
+        }
 
         public bool Selected {
             get { return _selected; }
             set {
-                _selected = value;
-                Debug.LogFormat("{0}.Selected = {1}", name, _selected);
+                if (_selected != value) {
+                    _selected = value;
+                    UpdateMaterial();
+                }
             }
+        }
+
+        void UpdateMaterial() {
+            if (_materialTarget)
+                _materialTarget.sharedMaterial = _selected ? SelectedMaterial : NormalMaterial;
         }
     }
 }
